157c79abc2c17f5cbc8fc002e53f484602065506
limjuchan <5320466@gmail.com>
2018-04-17 11:36:41 +0900

    2018-04-17.03 commit

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ console.log('hello world');
 const circle = require('./circle');
 console.log('Area of a circle = ' + circle.area(3));
 console.log('Circumference of circle =' + circle.circum(3));
-console.log('Pi =' + circle.pi);
+console.log('Pi = ' + circle.pi);
 
 const square = require('./square');
-let mySquare = square(4);
+let mySquare = square(5);
+mySquare.width = 10;
 console.log('Area of a square = ' +  mySquare.area());
+console.log('Circumference of a square' + mySquare.circum);
+
+
--- square.js
+++ square.js
@@ -1,7 +1,11 @@
-module.exports = function () {
+module.exports = function (width) {
     return {
+        width: width,
         area : function () {
             return width * width;
+        },
+        circum: function () {
+            return 4 * this.width; 
         }
     };
 }
