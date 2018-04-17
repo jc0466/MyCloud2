273cb76b5f74610c453d1f8baf675bc198a03e0e
limjuchan <5320466@gmail.com>
2018-04-17 12:35:08 +0900

    2018-04-17.04 commit

--- myServer.js
+++ myServer.js
@@ -3,7 +3,7 @@ const sHost = 'localhost';
 const nPort = 8000;
 
 function start() {
- function onRequest(req, res) {
+function onRequest(req, res) {
  console.log('Request received.');
  res.writeHead(200, { 'Content-Type': 'text/html' });
  res.write('Hello World!');
