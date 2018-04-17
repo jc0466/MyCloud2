f03c4b3d257949633b4b665a789e3cb5dda9d5df
limjuchan <5320466@gmail.com>
2018-04-17 10:58:47 +0900

    2018-04-17 commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log('hello world');
 const circle = require('./circle');
 console.log('Area of a circle = ' + circle.area(3));
+console.log('Circumference of circle =' + circle.circum)
+console.log('Pi =' + circle.pi);
+
+const square = require('./square');
+let mySquare = square(4);
+console.log('Area of a square = ' +  mySquare.area());
